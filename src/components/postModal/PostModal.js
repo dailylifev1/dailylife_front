a7b51ac83a847ce8c9901bf90502891645e670e0
Jimmy neutron <qny123@naver.com>
2022-09-01 03:43:49 +0900

    fix: eslint 오류 수정

--- src/components/postModal/PostModal.js
+++ src/components/postModal/PostModal.js
@@ -1,21 +1,21 @@
-import { useDispatch, useSelector } from "react-redux/es/exports";
-import { useEffect, useState } from "react";
-import styled from "styled-components";
-import axios from "axios";
-import { useNavigate } from "react-router-dom";
-import "./postModal.css";
-import { postActions } from "../../reducers/post";
+import { useDispatch, useSelector } from 'react-redux/es/exports';
+import { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import './postModal.css';
+import { postActions } from '../../reducers/post';
 
 const tempComment = [
   {
     id: 1,
-    content: "",
+    content: '',
     commentLike: 0,
     subCommentCount: 0,
   },
   {
     id: 2,
-    content: "테스트용 댓글입니다.",
+    content: '테스트용 댓글입니다.',
     commentLike: 3,
     subCommentCount: 2,
     subComments: [
@@ -26,13 +26,13 @@ const tempComment = [
   },
   {
     id: 3,
-    content: "",
+    content: '',
     commentLike: 1,
     subCommentCount: 0,
   },
 ];
 
-const ModalWindow = styled.div.attrs({ className: "ModalWindow" })`
+const ModalWindow = styled.div.attrs({ className: 'ModalWindow' })`
   position: fixed;
   background-color: rgba(0, 0, 0, 0.25);
   top: 0;
@@ -42,10 +42,10 @@ const ModalWindow = styled.div.attrs({ className: "ModalWindow" })`
   z-index: 999;
   visibility: visible;
   opacity: ${(props) => props.modalOpacity};
-  pointer-events: ${(props) => (props.modalOpacity === 0 ? "none" : "auto")};
+  pointer-events: ${(props) => (props.modalOpacity === 0 ? 'none' : 'auto')};
   transition: all 0.3s;
 
-  font-family: "Pretendard";
+  font-family: 'Pretendard';
   font-style: normal;
   font-weight: 400;
   font-size: 16px;
@@ -87,7 +87,7 @@ const Follow = styled.button`
   border: none;
   border-radius: 100px;
 
-  font-family: "Pretendard";
+  font-family: 'Pretendard';
   font-style: normal;
   font-weight: 400;
   font-size: 16px;
@@ -121,13 +121,13 @@ const ModalSocial = styled.div`
   align-items: center;
 `;
 const CommentContainer = styled.div.attrs({
-  className: "comment-container",
+  className: 'comment-container',
 })`
   & > .comment-date-container {
     display: flex;
   }
 `;
-const CommentMain = styled.div.attrs({ className: "comment-main" })`
+const CommentMain = styled.div.attrs({ className: 'comment-main' })`
   & > .username {
     margin-left: 10px;
     font-weight: 600;
@@ -142,7 +142,7 @@ const CommentMain = styled.div.attrs({ className: "comment-main" })`
   }
 `;
 const CommentDateContainer = styled.div.attrs({
-  className: ".comment-date-container",
+  className: '.comment-date-container',
 })`
   & > .empty {
     width: 38px;
@@ -160,18 +160,18 @@ const CommentDateContainer = styled.div.attrs({
     display: flex;
   }
 `;
-const CommentCreate = styled.div.attrs({ className: "comment-create" })`
+const CommentCreate = styled.div.attrs({ className: 'comment-create' })`
   & > .comment-create-text {
     border: 1px solid #dcdcdc;
     border-radius: 100px;
     width: 100%;
     margin-left: 10px;
     padding: 10px;
-    font-family: "Pretendard";
+    font-family: 'Pretendard';
     font-size: 16px;
   }
 `;
-const ReplyOption = styled.span.attrs({ className: "reply-option" })`
+const ReplyOption = styled.span.attrs({ className: 'reply-option' })`
   background: none;
   border: none;
   vertical-align: top;
@@ -184,7 +184,7 @@ const ReplyOption = styled.span.attrs({ className: "reply-option" })`
 `;
 
 function PostModal({ modalOpacity, setModalOpacity }) {
-  console.log("executes PostModal");
+  console.log('executes PostModal');
   const currentPostData = useSelector((state) => state.selectedPostData);
   const [replyList, setReplyList] = useState([]);
   const [replyHover, setReplyHover] = useState(-1);
@@ -193,12 +193,12 @@ function PostModal({ modalOpacity, setModalOpacity }) {
   const dispatch = useDispatch();
 
   const boardDelete = () => {
-    navigate("/");
+    navigate('/');
   };
 
   /** 댓글 작성 api 통신 함수 */
   function replyInsertHandler(e) {
-    if (localStorage.getItem("accessToken")) {
+    if (localStorage.getItem('accessToken')) {
       axios
         .post(
           `${process.env.REACT_APP_HOST}/api/reply/insert`,
@@ -208,33 +208,30 @@ function PostModal({ modalOpacity, setModalOpacity }) {
           },
           {
             headers: {
-              "X-ACCESS-TOKEN": localStorage.getItem("accessToken"),
+              'X-ACCESS-TOKEN': localStorage.getItem('accessToken'),
             },
-          }
+          },
         )
         .then((res) => {
-          e.target.value = "";
+          e.target.value = '';
           res.data.time = dateHandler(res.data.replyTime);
           setReplyList([...replyList, res.data]);
         })
         .catch((err) => console.log(err));
     } else {
-      e.target.value = "";
-      alert("로그인 후 이용해주세요.");
+      e.target.value = '';
+      alert('로그인 후 이용해주세요.');
     }
   }
 
   /** 게시글 선택 시 replyList(댓글 정보 state) 업데이트 */
   useEffect(() => {
     axios
-      .get(
-        `${process.env.REACT_APP_HOST}/api/reply/getReply/${currentPostData.boardNum}`,
-        {
-          headers: {
-            "X-ACCESS-TOKEN": localStorage.getItem("accessToken"),
-          },
-        }
-      )
+      .get(`${process.env.REACT_APP_HOST}/api/reply/getReply/${currentPostData.boardNum}`, {
+        headers: {
+          'X-ACCESS-TOKEN': localStorage.getItem('accessToken'),
+        },
+      })
       .then((res) => {
         console.log(res.data);
         res.data.map((item, idx) => {
@@ -251,35 +248,20 @@ function PostModal({ modalOpacity, setModalOpacity }) {
 
   /** 댓글이 작성된 날짜 계산 */
   const dateHandler = (replyDate) => {
-    const [sec, min, hour, day, week, month, year] = [
-      1,
-      60,
-      3600,
-      86400,
-      86400 * 7,
-      2592000,
-      2592000 * 12,
-    ];
+    const [sec, min, hour, day, week, month, year] = [1, 60, 3600, 86400, 86400 * 7, 2592000, 2592000 * 12];
 
     const today = new Date();
     replyDate = new Date(
-      `${replyDate[0]}-${replyDate[1]}-${replyDate[2]} ${
-        (replyDate[3] + 9) % 24
-      }:${replyDate[4]}:${replyDate[5]}`
+      `${replyDate[0]}-${replyDate[1]}-${replyDate[2]} ${(replyDate[3] + 9) % 24}:${replyDate[4]}:${replyDate[5]}`,
     );
-    const elapsedTime = Math.trunc(
-      (today.getTime() - replyDate.getTime()) / 1000
-    );
-    let elapsedText = "";
+    const elapsedTime = Math.trunc((today.getTime() - replyDate.getTime()) / 1000);
+    let elapsedText = '';
 
-    if (elapsedTime < sec) elapsedText = "지금";
+    if (elapsedTime < sec) elapsedText = '지금';
     else if (elapsedTime < min) elapsedText = `${elapsedTime}초`;
-    else if (elapsedTime < hour)
-      elapsedText = `${Math.trunc(elapsedTime / min)}분`;
-    else if (elapsedTime < day)
-      elapsedText = `${Math.trunc(elapsedTime / hour)}시간`;
-    else if (elapsedTime < week)
-      elapsedText = `${Math.trunc(elapsedTime / day)}일`;
+    else if (elapsedTime < hour) elapsedText = `${Math.trunc(elapsedTime / min)}분`;
+    else if (elapsedTime < day) elapsedText = `${Math.trunc(elapsedTime / hour)}시간`;
+    else if (elapsedTime < week) elapsedText = `${Math.trunc(elapsedTime / day)}일`;
     else elapsedText = `${Math.trunc(elapsedTime / week)}주`;
 
     return elapsedText;
@@ -289,21 +271,18 @@ function PostModal({ modalOpacity, setModalOpacity }) {
   const getReplyHeart = (replyNum) => {
     let replyHeart = 1;
     axios
-      .get(
-        `${process.env.REACT_APP_HOST}/api/heart/countHeartReply/${replyNum}`,
-        {
-          headers: {
-            "X-ACCESS-TOKEN": localStorage.getItem("accessToken"),
-          },
-        }
-      )
+      .get(`${process.env.REACT_APP_HOST}/api/heart/countHeartReply/${replyNum}`, {
+        headers: {
+          'X-ACCESS-TOKEN': localStorage.getItem('accessToken'),
+        },
+      })
       .then((res) => {
         replyHeart = res.data;
       })
       .catch((err) => console.log(err));
 
     if (replyHeart === 0) return null;
-    return "좋아요 " + replyHeart;
+    return '좋아요 ' + replyHeart;
   };
   return (
     <>
@@ -329,16 +308,10 @@ function PostModal({ modalOpacity, setModalOpacity }) {
                     title="title"
                     onClick={(e) => {
                       setModalOpacity(0);
-                      console.log("x clicked");
+                      console.log('x clicked');
                     }}
                   >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="18"
-                      height="18"
-                      viewBox="0 0 18 18"
-                      fill="none"
-                    >
+                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" fill="none">
                       <path
                         d="M1 1L17 17"
                         stroke="#42413C"
@@ -347,13 +320,7 @@ function PostModal({ modalOpacity, setModalOpacity }) {
                         strokeLinejoin="round"
                       />
                     </svg>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="18"
-                      height="18"
-                      viewBox="0 0 18 18"
-                      fill="none"
-                    >
+                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" fill="none">
                       <path
                         d="M1 1L17 17"
                         stroke="#42413C"
@@ -378,20 +345,16 @@ function PostModal({ modalOpacity, setModalOpacity }) {
                     onClick={() => {
                       console.log(currentPostData);
                       axios
-                        .delete(
-                          `${process.env.REACT_APP_HOST}/api/board/delete/${currentPostData.boardNum}`,
-                          {
-                            headers: {
-                              "X-ACCESS-TOKEN":
-                                localStorage.getItem("accessToken"),
-                            },
-                          }
-                        )
+                        .delete(`${process.env.REACT_APP_HOST}/api/board/delete/${currentPostData.boardNum}`, {
+                          headers: {
+                            'X-ACCESS-TOKEN': localStorage.getItem('accessToken'),
+                          },
+                        })
                         .then((res) => {
                           // console.log(res);
-                          alert("게시글이 성공적으로 삭제되었습니다.");
+                          alert('게시글이 성공적으로 삭제되었습니다.');
                           setModalOpacity(0);
-                          console.log("게시글이 성공적으로 삭제되었습니다.");
+                          console.log('게시글이 성공적으로 삭제되었습니다.');
                           // boardDelete();
 
                           // window.location.href = "/";
@@ -408,13 +371,7 @@ function PostModal({ modalOpacity, setModalOpacity }) {
               <ModalSocial>
                 <div className="social-icons-container">
                   <span className="likes-icon">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="22"
-                      height="20"
-                      viewBox="0 0 22 20"
-                      fill="none"
-                    >
+                    <svg xmlns="http://www.w3.org/2000/svg" width="22" height="20" viewBox="0 0 22 20" fill="none">
                       <path
                         d="M3.87499 11.625C8.37499 16.875 11 18.375 11 18.375C11 18.375 13.625 16.875 18.125 11.625C22.625 6.375 19.625 1.125 15.875 1.125C12.125 1.125 11 5.625 11 5.625C11 5.625 9.87499 1.125 6.12499 1.125C2.37499 1.125 -0.625012 6.375 3.87499 11.625Z"
                         stroke="#6A6A6A"
@@ -425,13 +382,7 @@ function PostModal({ modalOpacity, setModalOpacity }) {
                     </svg>
                   </span>
                   <span className="comments-icon">
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="22"
-                      height="21"
-                      viewBox="0 0 22 21"
-                      fill="none"
-                    >
+                    <svg xmlns="http://www.w3.org/2000/svg" width="22" height="21" viewBox="0 0 22 21" fill="none">
                       <path
                         d="M17.6525 14.8626C17.5228 14.9952 17.4233 15.1552 17.3613 15.3314C17.2993 15.5076 17.2762 15.6955 17.2937 15.8818C17.3816 16.7441 17.5482 17.5961 17.7912 18.4268C16.0475 18.0158 14.9825 17.5399 14.4987 17.2905C14.2244 17.149 13.9084 17.1155 13.6112 17.1963C12.7595 17.4275 11.8815 17.5439 11 17.5425C6.005 17.5425 2.25 13.9706 2.25 9.90747C2.25 5.84567 6.005 2.2725 11 2.2725C15.995 2.2725 19.75 5.84567 19.75 9.90747C19.75 11.7755 18.9787 13.5086 17.6525 14.8626ZM18.2687 19.8317C18.5649 19.8914 18.862 19.9462 19.16 19.9958C19.41 20.0365 19.6 19.7719 19.5012 19.5352C19.3903 19.2687 19.2885 18.9984 19.1962 18.7246L19.1925 18.7119C18.8825 17.7957 18.63 16.7421 18.5375 15.761C20.0712 14.1958 21 12.1471 21 9.90747C21 4.988 16.5225 1 11 1C5.4775 1 0.999998 4.988 0.999998 9.90747C0.999998 14.8269 5.4775 18.8149 11 18.8149C11.9904 18.8163 12.9768 18.6854 13.9337 18.4256C14.5837 18.7602 15.9825 19.3698 18.2687 19.8317Z"
                         fill="#6A6A6A"
@@ -443,9 +394,7 @@ function PostModal({ modalOpacity, setModalOpacity }) {
                 </div>
                 <div className="social-count-container">
                   <span className="likes-count">좋아요 {10}개</span>
-                  <span className="comments-count">
-                    댓글 {replyList.length}개
-                  </span>
+                  <span className="comments-count">댓글 {replyList.length}개</span>
                 </div>
               </ModalSocial>
               <hr />
@@ -466,13 +415,7 @@ function PostModal({ modalOpacity, setModalOpacity }) {
                       <div className="username">{item.userName}</div>
                       <div className="comment-content">{item.replyContext}</div>
                       <span className="comment-like">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="22"
-                          height="20"
-                          viewBox="0 0 22 20"
-                          fill="none"
-                        >
+                        <svg xmlns="http://www.w3.org/2000/svg" width="22" height="20" viewBox="0 0 22 20" fill="none">
                           <path
                             d="M3.87499 11.625C8.37499 16.875 11 18.375 11 18.375C11 18.375 13.625 16.875 18.125 11.625C22.625 6.375 19.625 1.125 15.875 1.125C12.125 1.125 11 5.625 11 5.625C11 5.625 9.87499 1.125 6.12499 1.125C2.37499 1.125 -0.625012 6.375 3.87499 11.625Z"
                             stroke="#6A6A6A"
@@ -486,18 +429,15 @@ function PostModal({ modalOpacity, setModalOpacity }) {
                     <CommentDateContainer>
                       <span className="empty"></span>
                       <span className="comment-date">
-                        {item.time} · {getReplyHeart(item.replyNum) ?? ""}
-                        {" · "}
+                        {item.time} · {getReplyHeart(item.replyNum) ?? ''}
+                        {' · '}
                         답글 달기
                         {item.subCommentCount ? (
                           <>
-                            <span className="comment-expand">
-                              {" "}
-                              · 답글 보기 ({item.subCommentCount}개){" "}
-                            </span>
+                            <span className="comment-expand"> · 답글 보기 ({item.subCommentCount}개) </span>
                           </>
                         ) : (
-                          ""
+                          ''
                         )}
                         {replyHover === item.replyNum ? (
                           <ReplyOption
@@ -521,7 +461,7 @@ function PostModal({ modalOpacity, setModalOpacity }) {
                             </svg>
                           </ReplyOption>
                         ) : (
-                          ""
+                          ''
                         )}
                       </span>
                       {/* 댓글 답변목록 */}
@@ -542,20 +482,16 @@ function PostModal({ modalOpacity, setModalOpacity }) {
                   className="comment-create-text"
                   placeholder="댓글 달기"
                   onKeyUp={(e) => {
-                    if (window.event.keyCode === 13 && e.target.value !== "")
-                      replyInsertHandler(e);
+                    if (window.event.keyCode === 13 && e.target.value !== '') replyInsertHandler(e);
                   }}
                 ></input>
               </CommentCreate>
             </div>
           </ModalBody>
           {replyDeleteFlag !== -1 ? (
-            <ReplyDeleteModal
-              replyDeleteNum={replyDeleteFlag}
-              setReplyDeleteFlag={setReplyDeleteFlag}
-            />
+            <ReplyDeleteModal replyDeleteNum={replyDeleteFlag} setReplyDeleteFlag={setReplyDeleteFlag} />
           ) : (
-            ""
+            ''
           )}
         </ModalWindow>
       </div>
@@ -568,7 +504,7 @@ function ReplyDeleteModal(props) {
     axios
       .delete(`${process.env.REACT_APP_HOST}/api/reply/delete/${replyNum}`, {
         headers: {
-          "X-ACCESS-TOKEN": localStorage.getItem("accessToken"),
+          'X-ACCESS-TOKEN': localStorage.getItem('accessToken'),
         },
       })
       .then((res) => console.log(res))
