565465354b6b05095307f9390de79bae91f829c7
NEARworld <artemismars2@outlook.com>
2022-09-01 22:03:03 +0900

    env: eslint 줄바꿈 경고 off

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,7 @@ module.exports = {
   },
   plugins: ['react'],
   rules: {
+    'react/jsx-one-expression-per-line': 'off',
     'no-alert': 'off',
     'react/react-in-jsx-scope': 'off',
     'linebreak-style': 0,
