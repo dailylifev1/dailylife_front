4004563db8bc1ac4246ba5f63d7f69c15703b57b
Jimmy neutron <qny123@naver.com>
2022-09-07 03:15:58 +0900

    feat: 회원가입 유효성 검사 로직 추가

--- src/components/signUp/useForm.js
+++ src/components/signUp/useForm.js
@@ -2,31 +2,41 @@ import { useState } from 'react';
 
 import userApi from '../../apis/userApi';
 
+import { validate } from 'common/utils';
+
 const useForm = (initialValues) => {
   const [values, setValues] = useState(initialValues);
+  const [errors, setErrors] = useState({});
+  // const [submitting, setSubmitting] = useState(false);
 
-  const handleChange = (event) => {
+  const handleChange = (event, formType) => {
     const { name, value } = event.target;
+
     setValues((prevState) => ({ ...prevState, [name]: value }));
-    // console.log(values);
+    setErrors((prevState) => ({ ...prevState, ...validate(values[formType], formType) }));
   };
 
   const handleSubmit = (event) => {
+    const isValidate = validate(values).length === 0;
+    if (isValidate) {
+      userApi.postUserInfoForSignUp({
+        userId: values.userId,
+        userPassword: values.password,
+        userEmail: values.email,
+        userName: values.name,
+        userPhoneNumber: values.phoneNumber,
+      });
+    } else {
+      setErrors(validate(values));
+    }
     event.preventDefault();
-
-    userApi.postUserInfoForSignUp({
-      userId: values.userId,
-      userPassword: values.password,
-      userEmail: values.email,
-      userName: values.name,
-      userPhoneNumber: values.phoneNumber,
-    });
   };
 
   return {
     values,
     handleChange,
     handleSubmit,
+    errors,
   };
 };
 
