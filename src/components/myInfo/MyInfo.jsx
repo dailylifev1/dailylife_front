c6ee3e317fc225629d8747a6c3bd31db4e5e7382
주정준 <65483521+jun1485@users.noreply.github.com>
2022-09-07 00:30:02 +0900

    chore: myInfoForm 분리

---
+++ src/components/myInfo/MyInfo.jsx
@@ -0,0 +1,8 @@
+import './myinfo.css';
+import MyInfoForm from './myInfoForm/myInfoForm';
+
+export default function MyInfo() {
+  return (
+    <MyInfoForm />
+  );
+}
--- src/components/myInfo/myInfoForm/myInfoForm.jsx
+++ src/components/myInfo/myInfoForm/myInfoForm.jsx
@@ -1,26 +1,24 @@
-import { type } from "@testing-library/user-event/dist/type";
-import axios from "axios";
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import { myInfoActions } from "../../reducers/myInfo";
-import "./myinfo.css";
+import axios from 'axios';
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
-export default function MyInfo() {
-  const userData = useSelector((state) => state.myInfo);
+import { myInfoActions } from 'reducers/myInfo';
+
+function MyInfoForm() {
   const dispatch = useDispatch();
+  const userData = useSelector((state) => state.myInfo);
 
-  console.log(userData);
   useEffect(() => {
-    if (localStorage.getItem("accessToken")) {
+    if (localStorage.getItem('accessToken')) {
       axios
         .post(
           `${process.env.REACT_APP_HOST}/api/users/details/${userData.userNum}`,
           null,
           {
             headers: {
-              "X-ACCESS-TOKEN": localStorage.getItem("accessToken"),
+              'X-ACCESS-TOKEN': localStorage.getItem('accessToken'),
             },
-          }
+          },
         )
         .then((res) => dispatch(myInfoActions.updateUserInfo(res.data.data)))
         .catch((err) => console.log(err));
@@ -28,7 +26,7 @@ export default function MyInfo() {
   }, []);
 
   return (
-    <div style={{ textAlign: "center", marginTop: "60px" }}>
+    <div style={{ textAlign: 'center', marginTop: '60px' }}>
       {userData.userInfo.userName}님의 회원 정보 페이지입니다.
       <div className="profile-container">
         <div className="user-email">
@@ -39,3 +37,5 @@ export default function MyInfo() {
     </div>
   );
 }
+
+export default MyInfoForm;
