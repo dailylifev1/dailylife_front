720cb0ea616a4a8fc67b901af57fbc86ab70ca3c
주정준 <65483521+jun1485@users.noreply.github.com>
2022-09-07 14:07:32 +0900

    chore: myInfo 전체적인 form 컴포넌트화 완료

--- src/components/myInfo/myInfoForm/useSetToken.js
+++ src/components/myInfo/myInfoForm/useSetToken.js
@@ -1,9 +1,6 @@
 import axios from 'axios';
 import { useEffect } from 'react';
-import {
-  useDispatch,
-  useSelector,
-} from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { myInfoActions } from 'reducers/myInfo';
 
@@ -12,10 +9,8 @@ function useSetToken() {
   const userData = useSelector(
     (state) => state.myInfo,
   );
-  // eslint-disable-next-line no-undef
   useEffect(() => {
     if (localStorage.getItem('accessToken')) {
-      // eslint-disable-next-line no-undef
       axios
         .post(
           `${process.env.REACT_APP_HOST}/api/users/details/${userData.userNum}`,
@@ -29,17 +24,14 @@ function useSetToken() {
             },
           },
         )
-        .then((res) =>
-          dispatch(
-            myInfoActions.updateUserInfo(
-              res.data.data,
-            ),
+        .then((res) => dispatch(
+          myInfoActions.updateUserInfo(
+            res.data.data,
           ),
-        )
+        ))
         .catch((err) => console.log(err));
     }
   }, []);
   return userData;
 }
-
 export default useSetToken;
