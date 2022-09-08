a6e91040fe79f7787ac62bbbe726b246901ae439
NEARworld <artemismars2@outlook.com>
2022-09-08 18:00:40 +0900

    refactor(dateHandler): formattedDate 추가

--- src/components/postModal/index.jsx
+++ src/components/postModal/index.jsx
@@ -28,8 +28,55 @@ function PostModal(
     [],
   );
   const replyInput = useRef();
+  /** 서버로부터 replyTime을 받아서 분/시간/일/주 같은 텍스트로 리턴 */
+  const dateHandler = (replyDate) => {
+    const [sec, min, hour, day, week] = [
+      1,
+      60,
+      3600,
+      86400,
+      86400 * 7,
+      2592000,
+      2592000 * 12,
+    ];
+    const today = new Date();
+    const formattedDate = new Date(
+      `${replyDate[0]}-${replyDate[1]}-${
+        replyDate[2]
+      } ${(replyDate[3] + 9) % 24}:${
+        replyDate[4]
+      }:${replyDate[5]}`,
+    );
+    const elapsedTime = Math.trunc(
+      (today.getTime() -
+        formattedDate.getTime()) /
+        1000,
+    );
+    let elapsedText = '';
 
-  /** 댓글 작성 api 통신 함수 */
+    if (elapsedTime < sec) elapsedText = '지금';
+    else if (elapsedTime < min)
+      elapsedText = `${elapsedTime}초`;
+    else if (elapsedTime < hour)
+      elapsedText = `${Math.trunc(
+        elapsedTime / min,
+      )}분`;
+    else if (elapsedTime < day)
+      elapsedText = `${Math.trunc(
+        elapsedTime / hour,
+      )}시간`;
+    else if (elapsedTime < week)
+      elapsedText = `${Math.trunc(
+        elapsedTime / day,
+      )}일`;
+    else
+      elapsedText = `${Math.trunc(
+        elapsedTime / week,
+      )}주`;
+
+    return elapsedText;
+  };
+  /** 서버로부터 댓글 정보를 받아서 replyList 상태에 저장 */
   function replyInsertHandler(e) {
     if (localStorage.getItem('accessToken')) {
       axios
@@ -44,7 +91,6 @@ function PostModal(
             },
           },
         )
-
         .then((res) => {
           e.target.value = '';
           res.data.time = dateHandler(
@@ -73,9 +119,9 @@ function PostModal(
       )
       .then((res) => {
         console.log(res.data);
-        res.data.map((item, idx) => {
-          item.time = dateHandler(item.replyTime);
-        });
+        res.data.map((item) =>
+          dateHandler(item.replyTime),
+        );
         setReplyList(res.data);
       })
       .catch((err) => console.log(err));
@@ -87,55 +133,6 @@ function PostModal(
       sessionStorage.removeItem('replyInfo');
     };
   }, [currentPostData.boardNum]);
-
-  /** 댓글이 작성된 날짜 계산 */
-  const dateHandler = (replyDate) => {
-    const [sec, min, hour, day, week] = [
-      1,
-      60,
-      3600,
-      86400,
-      86400 * 7,
-      2592000,
-      2592000 * 12,
-    ];
-
-    const today = new Date();
-    replyDate = new Date(
-      `${replyDate[0]}-${replyDate[1]}-${
-        replyDate[2]
-      } ${(replyDate[3] + 9) % 24}:${
-        replyDate[4]
-      }:${replyDate[5]}`,
-    );
-    const elapsedTime = Math.trunc(
-      (today.getTime() - replyDate.getTime()) /
-        1000,
-    );
-    let elapsedText = '';
-
-    if (elapsedTime < sec) elapsedText = '지금';
-    else if (elapsedTime < min)
-      elapsedText = `${elapsedTime}초`;
-    else if (elapsedTime < hour)
-      elapsedText = `${Math.trunc(
-        elapsedTime / min,
-      )}분`;
-    else if (elapsedTime < day)
-      elapsedText = `${Math.trunc(
-        elapsedTime / hour,
-      )}시간`;
-    else if (elapsedTime < week)
-      elapsedText = `${Math.trunc(
-        elapsedTime / day,
-      )}일`;
-    else
-      elapsedText = `${Math.trunc(
-        elapsedTime / week,
-      )}주`;
-
-    return elapsedText;
-  };
 
   /** 댓글 좋아요 개수 불러오는 api 통신 함수 */
   const getReplyHeart = (replyNum) => {
