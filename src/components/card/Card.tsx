96ab6ec0a106fdccc040f716fdea7aad66307af3
Lela12 <sharryun@gmail.com>
2022-09-25 00:13:08 +0900

    refactor: 타입스크립트 적용

---
+++ src/components/card/Card.tsx
@@ -0,0 +1,71 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { postActions } from '../../reducers/post';
+import PostModal from '../postModal/index';
+import CardItem from './CardItem';
+import './Cards.css';
+
+export type CardItemData = {
+  boardNum?: number;
+  src?: string;
+  title: string;
+  content: string;
+  heartState: number;
+  path: string;
+};
+
+export interface CardProps {
+  data: CardItemData;
+}
+
+function Cards() {
+  const dispatch = useDispatch();
+  const cardData = useSelector((state) => state.post);
+  const [modalOpacity, setModalOpacity] = useState<number>(0);
+
+  useEffect(() => {
+    async function fetchCards() {
+      const getCards = await axios
+        .get(`${process.env.REACT_APP_HOST}/api/board/getBoard`, {
+          headers: {
+            'X-ACCESS-TOKEN': localStorage.getItem('accessToken'),
+          },
+        })
+        .then((res) => res.data);
+
+      dispatch(postActions.updateItems(getCards));
+    }
+    fetchCards();
+  }, [modalOpacity]);
+
+  return (
+    <div className="cards">
+      {/* <Searching cardData={cardData} /> */}
+      <div className="cards__container">
+        <div className="cards__wrapper">
+          <div className="cards__items">
+            {cardData.myValues.map((data) => (
+              <CardItem
+                key={data.boardNum}
+                boardNum={data.boardNum}
+                src={data.serverFileUrl}
+                title={data.title}
+                content={data.content}
+                heartState={data.heart}
+                path={data.path}
+                setModalOpacity={setModalOpacity}
+              />
+            ))}
+            <PostModal
+              setModalOpacity={setModalOpacity}
+              modalOpacity={modalOpacity}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+export default Cards;
