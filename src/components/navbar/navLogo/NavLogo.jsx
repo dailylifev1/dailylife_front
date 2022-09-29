44b9b262699c71c15e301d3b10957d2d03753bcb
주정준 <65483521+jun1485@users.noreply.github.com>
2022-09-29 15:54:13 +0900

    feat: 검색 라우터 링크 추가, 메인로고 클릭 시 메인화면으로 이동 구현

--- src/components/navbar/navLogo/NavLogo.jsx
+++ src/components/navbar/navLogo/NavLogo.jsx
@@ -1,11 +1,17 @@
 import { Link } from 'react-router-dom';
 
 import MainLogoIcon from 'components/Icons/MainLogoIcon';
+import { useAppDispatch, useAppSelector } from 'store/hooks';
+import { postActions } from 'reducers/post';
 
 function NavLogo() {
+  const store = useAppSelector((state => state.post))
+  const dispatch = useAppDispatch();
   return (
     <div className="nav-header">
-      <button className="nav-title" type="button">
+      <button className="nav-title" onClick={() => {
+        dispatch(postActions.updateIsLogoClicked(!store.isLogoClicked));
+      }} type="button">
         <Link to="/">
           <MainLogoIcon />
         </Link>
