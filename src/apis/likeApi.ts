24df90c705b504f5c3b55d0491d557d99a2ea385
NEARworld <artemismars2@outlook.com>
2022-10-02 16:08:31 +0900

    feat: likeApi 생성

---
+++ src/apis/likeApi.ts
@@ -0,0 +1,42 @@
+import { methodFormat } from 'common/utils';
+import client from './client';
+
+const PATH = `api/heart`;
+
+const likeApi = {
+  /** 게시글 좋아요 업데이트 */
+  updateBoardHeart: methodFormat(
+    async (boardNum: number, accessToken: string) => {
+      const response = await client.post(
+        `${PATH}/boardHeartPlus`,
+        {
+          boardNum,
+        },
+        {
+          headers: {
+            'X-ACCESS-TOKEN': accessToken,
+          },
+        },
+      );
+      return response.data;
+    },
+  ),
+  updateCommentHeart: methodFormat(
+    async (replyNum: number, accessToken: string) => {
+      const response = await client.post(
+        `${PATH}/replyHeartPlus`,
+        {
+          replyNum,
+        },
+        {
+          headers: {
+            'X-ACCESS-TOKEN': accessToken,
+          },
+        },
+      );
+      return response.data;
+    },
+  ),
+};
+
+export default likeApi;
