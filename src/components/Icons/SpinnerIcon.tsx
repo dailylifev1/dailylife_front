a9355e4b0309ed1efa85f34dce07ce13084a001d
NEARworld <artemismars2@outlook.com>
2022-10-02 16:57:49 +0900

    refactor: Icons

--- src/components/Icons/SpinnerIcon.tsx
+++ src/components/Icons/SpinnerIcon.tsx
@@ -1,7 +1,5 @@
-/* eslint-disable react/style-prop-object */
 function Spinner() {
   return (
-    // <? xml version = "1.0" encoding = "utf-8" ?>
     <svg
       xmlns="http://www.w3.org/2000/svg"
       xmlnsXlink="http://www.w3.org/1999/xlink"
@@ -14,9 +12,9 @@ function Spinner() {
         'animationDelay': '0s',
         width: '100px',
         height: '100px',
-        viewBox: '0 0 100 100',
-        preserveAspectRatio: 'xMidYMid',
       }}
+      preserveAspectRatio='xMidYMid'
+      viewBox='0 0 100 100'
     >
       <circle cx="50" cy="50" fill="none" stroke="#fcc400" strokeWidth="9" r="34" strokeDasharray="160.22122533307947 55.40707511102649" style={{ 'animationPlayState': 'running', 'animationDelay': '0s' }}>
         <animateTransform attributeName="transform" type="rotate" repeatCount="indefinite" dur="0.819672131147541s" values="0 50 50;360 50 50" keyTimes="0;1" style={{ 'animationPlayState': 'running', 'animationDelay': '0s' }} />
